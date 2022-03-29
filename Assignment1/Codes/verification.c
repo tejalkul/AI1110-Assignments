169d4f9c465adb5a2fec3291809a4a5619f719bf
tejalkul <102573760+tejalkul@users.noreply.github.com>
2022-03-29 12:41:51 +0530

    Add files via upload

---
+++ Assignment1/Codes/verification.c
@@ -0,0 +1,57 @@
+/*
+Name : Tejal Kulkarni
+Roll Number:CS21BTECH11058
+Date: 29/03/2022
+
+C code to find VAT paid by shopkeeper to government and total amount paid by customer(including tax) 
+given marked price,discount for shopkeeper,discount for customer and sales tax
+*/ 
+
+#include<stdio.h>
+
+//Finds the result when we find percent of a number
+int Percentage(int number,int percent)
+{
+    int result;
+    result = (number*percent)/100;
+    return result;
+}
+
+//Verification to get VAT paid by shopkeeper to government and amount paid by customer(including tax)
+void Verification(int marked_price,int sales_tax,int disc_shopkeeper,int disc_customer)
+{
+    int selling_prince_shopkeeper;
+    int selling_price_customer;
+    int discamount_shopkeeper;
+    int discamount_customer;
+    int tax_shopkeeper;
+    int tax_customer;
+    int VAT;
+    int Amount_customer;
+
+    discamount_shopkeeper = Percentage(marked_price,disc_shopkeeper);
+    selling_prince_shopkeeper = marked_price - discamount_shopkeeper;
+    tax_shopkeeper = Percentage(selling_prince_shopkeeper,sales_tax);
+
+    discamount_customer = Percentage(marked_price,disc_customer);
+    selling_price_customer = marked_price - discamount_customer;
+    tax_customer = Percentage(selling_price_customer,sales_tax);
+
+    VAT = tax_customer - tax_shopkeeper;
+    printf("VAT paid to government by shopkeeper = Rs.%d\n",VAT);    //output
+
+    Amount_customer = selling_price_customer + tax_customer;
+    printf("Amount paid by customer inclusive of tax = Rs.%d\n",Amount_customer);    //output
+
+}
+
+int main()
+{
+      //input
+      int marked_price = 45000;
+      int sales_tax = 12;
+      int disc_shopkeeper = 10;
+      int disc_customer = 5;  
+
+      Verification(marked_price,sales_tax,disc_shopkeeper,disc_customer);
+}

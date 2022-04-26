62224ec6f8f9dc0a90db133d6be7f6aaa14a0431
tejalkul <102573760+tejalkul@users.noreply.github.com>
2022-04-26 17:22:34 +0530

    Add files via upload

--- Assignment3/codes/probability.py
+++ Assignment3/codes/probability.py
@@ -1,6 +1,6 @@
 #Name : Tejal Kulkarni
 #Roll Number:CS21BTECH11058
-#Date: 24/04/2022
+#Date: 10/04/2022
 
 #Python Code to find probability of getting 2 heads in a 3 coin tossing experiment
 
@@ -8,6 +8,9 @@ import pandas as pd
 import numpy as np
 from numpy import linalg as LA, r_
 from numpy import random as RN
+
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import stem
 
 
 read = pd.read_excel('C:\\Users\\Tejal Kulkarni\\OneDrive\\Documents\\IITH 1st year books\\table1.xlsx')
@@ -36,6 +39,23 @@ pr_1 = 1.0*ctr_1/N
 pr_0 = 1.0*ctr_0/N
 
 #Display probability
-print("Probability of getting 2 Heads(Theoretical): ",Pr_two_heads)
+print("Probability of getting 2 Heads(Theorotical): ",Pr_two_heads)
 print("Probability of getting 2 Heads(Practical): ", pr_2)
+
+#Plotting PMF from the given data
+X = np.array([3,2,1,0])
+Y = np.array([frequency[1]/N,frequency[2]/N,frequency[3]/N,frequency[4]/N])
+fig1 = stem(X, Y, linefmt='k-', markerfmt='ko', basefmt='k-')
+plt.grid('minor')
+plt.savefig("C:\\Users\\Tejal Kulkarni\\OneDrive\\Pictures\\Saved Pictures\\PMF1.png")
+
+#Plotting PMF to compare theoretical and practical
+X = np.array([0,1,2,3])
+Y = np.array([1/8,3/8,3/8,1/8])
+fig2 = stem(X, Y, linefmt='k-', markerfmt='ro', basefmt='k-')
+plt.grid('minor')
+plt.text(2.2,0.32,"o - Theoretical",color = 'red')
+plt.text(2.2,0.3,"o - Practical",color = 'black')
+plt.savefig("C:\\Users\\Tejal Kulkarni\\OneDrive\\Pictures\\Saved Pictures\\PMF2.png")
+
 

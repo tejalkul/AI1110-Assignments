8452ab61b7adb01a716456ab011fe36f26640b9d
tejalkul <102573760+tejalkul@users.noreply.github.com>
2022-07-01 12:12:47 +0530

    Update cdf_plot.py

--- AI1110 Random Variables Assignment/codes/cdf_plot.py
+++ AI1110 Random Variables Assignment/codes/cdf_plot.py
@@ -13,9 +13,9 @@ x = np.linspace(-4,4,30)#points on the x axis
 simlen = int(1e6) #number of samples
 err = [] #declaring probability list
 #randvar = np.random.normal(0,1,simlen)
-#randvar = np.loadtxt('uni.dat',dtype='double')
+randvar = np.loadtxt('uni.dat',dtype='double')
 #randvar = np.loadtxt('gau.dat',dtype='double')
-randvar = np.loadtxt('log_V.dat',dtype='double')
+#randvar = np.loadtxt('log_V.dat',dtype='double')
 for i in range(0,30):
 	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
 	err_n = np.size(err_ind) #computing the probability
